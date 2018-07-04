f02faced67324504e7ed0b77957f474ad83f5242
Lucas Natoli <lucas.natoli@gmail.com>
2018-07-04 18:08:12 -0300

    ignore watch pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
       name      : 'web-server',
       script    : './index.js',
       watch     : true,
+      ignore_watch : ["node_modules", "docs", ".sqlite3", ".git"],
       instance_var: 'INSTANCE_ID',
       env: {
         PORT    : 3000,
